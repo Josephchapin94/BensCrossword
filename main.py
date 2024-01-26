27c8c68c59e41e6f3e1e79fb32547ec3f60e7c0a
Joseph Chapin <josephsarah0603@gmail.com>
2024-01-25 19:55:02 -0600

    Was able to get a grid around the text working on getting words to be vertical

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ class CrosswordGenerator:
         self.size = size
         self.grid = [[' ' for _ in range(size)] for _ in range(size)]
         self.words = []
+        self.words_given = set()
 
-    def add_word(self, word):
+    def add_word(self, word, is_given=False):
         word = word.upper()
         for _ in range(100):  # Try 100 times to place the word
             row = random.randint(0, self.size - 1)
@@ -16,9 +17,13 @@ class CrosswordGenerator:
 
             if direction == 'horizontal' and self.can_place_word_horizontal(word, row, col):
                 self.place_word_horizontal(word, row, col)
+                if is_given:
+                    self.words_given.add(word)  # Add word to words_given set
                 return True
             elif direction == 'vertical' and self.can_place_word_vertical(word, row, col):
                 self.place_word_vertical(word, row, col)
+                if is_given:
+                    self.words_given.add(word)  # Add word to words_given set
                 return True
 
         return False
@@ -27,7 +32,7 @@ class CrosswordGenerator:
         if col + len(word) > self.size:
             return False
         for i in range(len(word)):
-            if self.grid[row][col + i] not in (' ', word[i]):
+            if not (self.grid[row][col + i] == ' ' or self.grid[row][col + i] == word[i]):
                 return False
         return True
 
@@ -35,19 +40,19 @@ class CrosswordGenerator:
         if row + len(word) > self.size:
             return False
         for i in range(len(word)):
-            if self.grid[row + i][col] not in (' ', word[i]):
+            if not (self.grid[row + i][col] == ' ' or self.grid[row + i][col] == word[i]):
                 return False
         return True
 
     def place_word_horizontal(self, word, row, col):
         for i in range(len(word)):
             self.grid[row][col + i] = word[i]
-        self.words.append(word)
+        self.words.append((word, row, col, 'horizontal'))
 
     def place_word_vertical(self, word, row, col):
         for i in range(len(word)):
             self.grid[row + i][col] = word[i]
-        self.words.append(word)
+        self.words.append((word, row, col, 'vertical'))
 
     def fill_empty_spaces(self):
         for i in range(self.size):
@@ -59,8 +64,8 @@ class CrosswordGenerator:
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font('Arial', size=13)
-        
-         # Get the width of the text
+
+        # Get the width of the text
         text_width = pdf.get_string_width(text)
 
         # Calculate the x-coordinate to center the text
@@ -71,14 +76,21 @@ class CrosswordGenerator:
         pdf.cell(0, 10, text, ln=True)
 
         # Add crossword grid and key (these lines now also use centered text)
-        pdf.cell(0, 6, ln=True, align='C')
+        grid_width = len(self.grid[0]) * 6
+        x_coordinate_grid = (pdf.w - grid_width) / 2
+
         for row in self.grid:
-            line = ' '.join(row)
-            pdf.cell(0, 6, txt=line, ln=True, align='C')
+            pdf.set_x(x_coordinate_grid)
+            for cell in row:
+                pdf.cell(6, 6, cell, border=1)
+            pdf.ln(6)
+
+        pdf.ln(6)  # Add some space between the grid and the words
+
         pdf.cell(0, 6, txt="Key:", ln=True, align='C')
-        for word in self.words:
+        for word, _, _, _ in self.words:
             pdf.cell(0, 6, txt=word, ln=True, align='C')
-    
+
         pdf.output(filename)
 
 # Prompt user for words
@@ -100,4 +112,4 @@ for word in words_to_add:
     if not generator.add_word(word):
         print(f"Warning: Couldn't place the word '{word}' in the puzzle.")
 generator.fill_empty_spaces()
-generator.create_pdf("crossword.pdf", "Crossword Puzzle:" )
+generator.create_pdf("crossword.pdf", "Crossword Puzzle:")
